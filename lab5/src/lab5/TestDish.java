a5dc1f3ef1c95b521b5c8bc27da8e5d5ab05e434
Dukhanov9 <55548616+Dukhanov9@users.noreply.github.com>
2019-12-16 20:58:09 +0300

    Add files via upload

---
+++ lab5/src/lab5/TestDish.java
@@ -0,0 +1,10 @@
+package dish;
+
+import lab5.tarelka;
+
+public class TestDish {
+    public static void main(String[] args) {
+        tarelka t = new tarelka("стекло", "оранжевый", "большая");
+        System.out.println(t);
+    }
+}
